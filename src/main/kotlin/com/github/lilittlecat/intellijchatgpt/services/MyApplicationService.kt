2ad4c350adbf8326aca584b33ce198b025b7a627
GitHub Action <action@github.com>
2022-12-08 15:11:21 +0000

    Template cleanup

--- src/main/kotlin/com/github/lilittlecat/intellijchatgpt/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/lilittlecat/intellijchatgpt/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.lilittlecat.intellijchatgpt.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.lilittlecat.intellijchatgpt.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/lilittlecat/intellijchatgpt/services/MyProjectService.kt
+++ src/main/kotlin/com/github/lilittlecat/intellijchatgpt/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.lilittlecat.intellijchatgpt.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.lilittlecat.intellijchatgpt.MyBundle
 
 class MyProjectService(project: Project) {
 
